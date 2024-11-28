414e8609f816ba1c6c8ae716940a4237a27498dc
zhangzehao <1064771680@qq.com>
2024-11-28 09:40:34 +0800

    项目主页面搭建完成  更新配置文件 进入debug 模式
    # Conflicts:
    #	.idea/gradle.xml
    #	.idea/misc.xml
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/layout/activity_main.xml
    #	app/src/main/res/values/colors.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/styles.xml
    #	build.gradle
    #	gradle.properties
    #	gradle/wrapper/gradle-wrapper.properties
    #	gradlew
    #	gradlew.bat
    #	settings.gradle

--- app/src/main/java/com/bytedance/tiktok/bean/DataCreate.kt
+++ app/src/main/java/com/bytedance/tiktok/bean/DataCreate.kt
@@ -50,10 +50,10 @@ class DataCreate {
 
             val  videoData =     mutableNumbers.random()
             val videoBean = VideoBean()
-            type.add(videoData.videos["tiny"]?.url.toString())
+//            type.add(videoData.videos["tiny"]?.url.toString())
             videoBean.content = "# "+videoData.type+ "# "+videoData.tags
-//            videoBean.videoRes = videoData.videos["tiny"]?.url.toString()
-            videoBean.videoRes = type.random()
+            videoBean.videoRes = videoData.videos["tiny"]?.url.toString()
+//            videoBean.videoRes = type.random()
             videoBean.distance = 7.9f
             videoBean.isFocused = false
             videoBean.isLiked = true
@@ -91,7 +91,7 @@ class DataCreate {
         @JvmField
         var mutableNumbers = mutableSetOf<TestNetworkApi.Bean.HitsData>()
 
-        var type = mutableSetOf<String>()
+//        var type = mutableSetOf<String>()
 
 
         fun addData(mutableNumbers : MutableList<TestNetworkApi.Bean.HitsData>){
@@ -99,10 +99,10 @@ class DataCreate {
 
                 val  videoData =     mutableNumbers.random()
                 val videoBean = VideoBean()
-                type.add(videoData.videos["tiny"]?.url.toString())
+//                type.add(videoData.videos["tiny"]?.url.toString())
                 videoBean.content = "# "+videoData.type+ "# "+videoData.tags
-//                videoBean.videoRes = videoData.videos["tiny"]?.url.toString()
-                videoBean.videoRes = type.random()
+                videoBean.videoRes = videoData.videos["tiny"]?.url.toString()
+//                videoBean.videoRes = type.random()
                 videoBean.coverRes=  videoData.videos["tiny"]?.thumbnail.toString()
                     videoBean.distance = 7.9f
                 videoBean.isFocused = false
